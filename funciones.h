ae471211609e78df6ebfdce76603a32997026283
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-06-06 15:38:39 -0500

    inicio

---
+++ funciones.h
@@ -0,0 +1,4 @@
+int ingreseCedula();
+int obtenerPosicionContacto(int cedula[5],int cedulaBuscar);
+void imprimirContactos(char nombre[5][50],int telefonos[10][2],int indice);
+void imprimirDirecciones(char direcciones[10][2][50],int indice);
