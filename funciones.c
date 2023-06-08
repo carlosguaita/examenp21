3b2827ca44ddfa73d72f7d618879b3d254942d7a
carlosguaita <120821872+carlosguaita@users.noreply.github.com>
2023-06-08 15:37:24 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,1 +1,45 @@
 
+#include <stdio.h>
+#include "funciones.h"
+
+int ingreseCedula(){
+    int cedula;
+    printf("Ingrese la cedula del contacto: ");
+    scanf("%d",&cedula);
+    return cedula;
+}
+
+int obtenerPosicionContacto(int cedula[5],int cedulaBuscar){
+    int indice=0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (cedula[i]==cedulaBuscar)
+        {
+            indice=i;
+        }
+        
+    }
+    return indice;
+}
+
+void imprimirContactos(char nombre[5][50],int telefonos[10][2],int indice){
+    printf("Los telefonos asociados a %s\n",nombre[indice]);
+    for (int i = 0; i < 10; i++)
+    {
+        if (telefonos[i][0]==indice)
+        {
+            printf("%d\n",telefonos[i][1]);
+        }
+    }
+}
+
+void imprimirDirecciones(char direcciones[10][2][50],int indice){
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (direcciones[i][0][0] - '0' ==indice)
+        {
+            printf("%s\n",direcciones[i][1]);
+        }
+    }
+}
