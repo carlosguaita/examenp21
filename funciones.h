574a4bf0a53bf8ac0525da37137d6bc5d6759999
carlosguaita <120821872+carlosguaita@users.noreply.github.com>
2023-06-08 14:46:14 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,4 +1,1 @@
-int ingreseCedula();
-int obtenerPosicionContacto(int cedula[5],int cedulaBuscar);
-void imprimirContactos(char nombre[5][50],int telefonos[10][2],int indice);
-void imprimirDirecciones(char direcciones[10][2][50],int indice);
+
